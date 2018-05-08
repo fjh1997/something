630f8c2cc1511475aecf7e1529934ad47f3e5db4
549308442@qq.com <549308442@qq.com>
2018-05-08 12:59:04 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# something
-I have programmed somthing to deal with some issues in daily life 
+# 校园网openwrt自动拨号器 #
+  校园网安装路由，刷入openwrt，通过l2tP协议成功登入校园网，但是有时会出现15分钟左右掉线的情况，每次都要打开浏览器192.168.1.1访问路由器拨号，于是打算完善一下路由器的功能，但路由器里面的代码看不太懂，转念一想不如用一个笨办法，简单粗暴，使用爬虫替我拨号不就行了？？
+
+## 一、安装python27 ##
+  注意其中用到的splinter的python包只适用python27，python3不行
+## 二、安装火狐浏览器 ##
+  这个不用多讲
+## 三、安装pip ##
+  pip是python包管理器，一般是自带的，如果没有也可参考别人教程https://blog.csdn.net/huhuang/article/details/60873882
+## 四、使用pip安装splinter爬虫工具 ##
+  按住shift+鼠标右键，可以在任一目录下打开cmd，在命令行里输入
+    pip install splinter
+  如果报错，那可能是pip或者python的环境变量没设置好
+## 五、下载geckodriver.exe ##
+  这个在我的版本库中有，请自行下载，并放置在和python同一个环境变量的目录下
+## 六、修改python脚本 ##
+  用记事本打开post.js看里面的注释自行修改
+## 六、启动python脚本 ##
+	 python post.js
+  可以根据浏览器的实际情况更改脚本，如果还有问题（很大概率），尝试使用启动图形化界面的脚本，一步步分析哪里错了。
--- post.py
+++ post.py
@@ -6,24 +6,26 @@ from splinter import Browser
 def splinter(url):
 	browser = Browser('firefox',headless=True)
 	#browser = Browser()
-	#login 126 email websize
+	#以上两个方式选择是否调用图形化界面的浏览器帮你拨号，第一种带图形化界面，用于测试问题
 	
 	browser.visit(url)
-    #wait web element loading
+    #等待网页加载
 	time.sleep(0.5)
-	#fill in account and password
+	#加载后出现登录页面，需要输入密码才能访问
 	browser.find_by_name('luci_password').fill('root')
-     #click the button of login
+     #点击登录按钮
 	browser.find_by_value('登录').click()
 	time.sleep(0.5)
-	#close the window of brower
+	#以下启动一个无限循环用于检测是否掉线
 	def fn():
 		x=hash(browser.find_by_id('ZSTUVPN-ifc-description').first.value)
+		#这里的id要根据你opwnwrt网页上的html元素修改，使用开发人员工具（基本所有浏览器都有）查看，主要是看返回的网络连接信息的hash值和网络连接失败的信息的hash值是否一样。
 		if(x==-174366572):browser.execute_script("iface_shutdown('ZSTUVPN', true)")
-		thd.Timer(10,fn).start()
+		#在网页上调用javascript接口，相当于点击了连接按钮，后面的script也是用开发人员工具在网页上查看，把连接按钮组件onclick=“”这里面的函数复制下来
+		thd.Timer(10,fn).start()#设置每十秒检测一次
 	fn()
     
     
 if __name__ == '__main__':
-    websize ='http://192.168.1.1/cgi-bin/luci/admin/network/network'
+    websize ='http://192.168.1.1/cgi-bin/luci/admin/network/network'#设置成你要登录的网页
     splinter(websize)
